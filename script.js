376f8aef1104d3ce7615c7e26aeb98e65dbf4edd
Your Name <nedeltoga@gmail.com>
2022-05-12 18:20:24 +0200

    JS function createListSong fixes

--- script.js
+++ script.js
@@ -13,40 +13,53 @@ fetchSongs();
 
 
 function createListSong(song){
+    
+    // crear el ol
+    let card = document.createElement('ol');
+    card.classList.add('song-card');
+
     for(let i=0; i<song.length; i++){
         //crear título de la lista
         let listTitle = document.createElement('h3');
         listTitle.classList.add('list-title');
-        // crear el ol
-        let card = document.createElement('ol');
-        card.classList.add('song-card');
+
+        
+        
         // crear li
         let newList = document.createElement('li');
         newList.classList.add('element-list');
+        
         // crear icono
         let iconList = document.createElement('i');
         //iconList.src = <i class="fa-solid fa-play"></i>
+        
         // traer el nombre de la canción
         const name = document.createElement('p');
         name.classList.add('name');
         name.textContent = song[i].name;
+        
         //traer el nombre del grupo
         let newGroupNameSong = document.createElement('p');
         newGroupNameSong.classList.add('nameGroup-list');
+        newGroupNameSong.textContent = song[i].artist.name
+        
         // traer número de escuchas
         let newListeners = document.createElement('p');
         newListeners.classList.add('listeners-list');
+
         
-        card.appendChild(newList);
-        card.appendChild(h3);
         newList.appendChild(iconList);
+        newList.appendChild(newGroupNameSong)
         newList.appendChild(name);
-        newListg.appendChild(newGroupNameSong)
         newList.appendChild(newListeners);
 
-        listSong.appendChild(listTitle)
+        card.appendChild(newList);
+        card.appendChild(listTitle);
+
+       
 
     }
+    listSong.appendChild(card)
 }
 
 
